55c318e2c763b1fccc3384ff5a45b7503c9e270a
Bunny67 <zolov67@yandex.ru>
2016-08-18 23:35:04 +0300

    update

--- ElvUI_FriendsListColor/ElvUI_FriendsListColor.lua
+++ ElvUI_FriendsListColor/ElvUI_FriendsListColor.lua
@@ -5,20 +5,18 @@ local mod = E:NewModule("FriendsListColor", "AceHook-3.0");
 
 local _G = _G;
 local unpack, pairs = unpack, pairs;
-local format = string.format;
 
 local GetFriendInfo = GetFriendInfo;
 local GetNumFriends = GetNumFriends;
 local GetQuestDifficultyColor = GetQuestDifficultyColor;
 local CUSTOM_CLASS_COLORS = CUSTOM_CLASS_COLORS;
 local FRIENDS_BUTTON_TYPE_WOW = FRIENDS_BUTTON_TYPE_WOW;
-local FRIENDS_LEVEL_TEMPLATE = FRIENDS_LEVEL_TEMPLATE;
 local LOCALIZED_CLASS_NAMES_FEMALE = LOCALIZED_CLASS_NAMES_FEMALE;
 local LOCALIZED_CLASS_NAMES_MALE = LOCALIZED_CLASS_NAMES_MALE;
 local RAID_CLASS_COLORS = RAID_CLASS_COLORS;
 
 local locale = GetLocale();
-local test = locale == "ruRU" and ", %s%d-го уровня" or locale == "deDE" and ", %sStufe %d" or ", %sLevel $d";
+local textFormat = locale == "ruRU" and "%s%s|r, %s%s-го уровня" or locale == "deDE" and "%s%s|r, %sStufe %s" or "%s%s|r, %sLevel $s";
 
 local locclasses = {};
 for k, v in pairs(LOCALIZED_CLASS_NAMES_MALE) do locclasses[v] = k; end
@@ -34,14 +32,14 @@ function mod:FriendsList_Update()
 			color = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[locclasses[class]] or RAID_CLASS_COLORS[locclasses[class]];
 			diffColor = GetQuestDifficultyColor(level);
 			if(connected) then
-				friend.name:SetText(format("%s%s|r", E:RGBToHex(color.r, color.g, color.b), name) .. format(test, E:RGBToHex(diffColor.r, diffColor.g, diffColor.b), level));
+				friend.name:SetFormattedText(textFormat, E:RGBToHex(color.r, color.g, color.b), name, E:RGBToHex(diffColor.r, diffColor.g, diffColor.b), level);
 				ElvCharacterDB.FriendsListColor[name] = {level, class, zone};
 			else
 				if(ElvCharacterDB.FriendsListColor[name]) then
 					level, class, zone = unpack(ElvCharacterDB.FriendsListColor[name]);
 					color = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[locclasses[class]] or RAID_CLASS_COLORS[locclasses[class]];
 					diffColor = GetQuestDifficultyColor(level);
-					friend.name:SetText(format("%s%s|r", E:RGBToHex(color.r*0.5, color.g*0.5, color.b*0.5), name) .. format(test, E:RGBToHex(diffColor.r*0.5, diffColor.g*0.5, diffColor.b*0.5), level));
+					friend.name:SetFormattedText(textFormat, E:RGBToHex(color.r*0.5, color.g*0.5, color.b*0.5), name, E:RGBToHex(diffColor.r*0.5, diffColor.g*0.5, diffColor.b*0.5), level);
 					friend.info:SetText(zone);
 				end
 			end
