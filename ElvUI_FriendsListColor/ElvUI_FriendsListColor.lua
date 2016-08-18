388dd1d0e3e9bb76b98c7885a7578ffddf922b39
Bunny67 <zolov67@yandex.ru>
2016-08-18 23:12:01 +0300

    update
    So far, there are no ideas how to change color the text level :(

--- ElvUI_FriendsListColor/ElvUI_FriendsListColor.lua
+++ ElvUI_FriendsListColor/ElvUI_FriendsListColor.lua
@@ -12,10 +12,10 @@ local GetNumFriends = GetNumFriends;
 local GetQuestDifficultyColor = GetQuestDifficultyColor;
 local CUSTOM_CLASS_COLORS = CUSTOM_CLASS_COLORS;
 local FRIENDS_BUTTON_TYPE_WOW = FRIENDS_BUTTON_TYPE_WOW;
+local FRIENDS_LEVEL_TEMPLATE = FRIENDS_LEVEL_TEMPLATE;
 local LOCALIZED_CLASS_NAMES_FEMALE = LOCALIZED_CLASS_NAMES_FEMALE;
 local LOCALIZED_CLASS_NAMES_MALE = LOCALIZED_CLASS_NAMES_MALE;
 local RAID_CLASS_COLORS = RAID_CLASS_COLORS;
-local TOOLTIP_UNIT_LEVEL_CLASS = TOOLTIP_UNIT_LEVEL_CLASS;
 
 local locclasses = {};
 for k, v in pairs(LOCALIZED_CLASS_NAMES_MALE) do locclasses[v] = k; end
@@ -29,16 +29,16 @@ function mod:FriendsList_Update()
 		if(friend.id and friend.buttonType == FRIENDS_BUTTON_TYPE_WOW) then
 			name, level, class, zone, connected, status, note = GetFriendInfo(friend.id);
 			color = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[locclasses[class]] or RAID_CLASS_COLORS[locclasses[class]];
-			diffColor = GetQuestDifficultyColor(level);
+			--diffColor = GetQuestDifficultyColor(level);
 			if(connected) then
-				friend.name:SetText(format("%s%s|r", E:RGBToHex(color.r, color.g, color.b), name) .. "," .. format(TOOLTIP_UNIT_LEVEL_CLASS, level, E:RGBToHex(diffColor.r, diffColor.g, diffColor.b)));
+				friend.name:SetText(format("%s%s|r", E:RGBToHex(color.r, color.g, color.b), name) .. ", " .. format(FRIENDS_LEVEL_TEMPLATE, level, class));
 				ElvCharacterDB.FriendsListColor[name] = {level, class, zone};
 			else
 				if(ElvCharacterDB.FriendsListColor[name]) then
 					level, class, zone = unpack(ElvCharacterDB.FriendsListColor[name]);
 					color = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[locclasses[class]] or RAID_CLASS_COLORS[locclasses[class]];
-					diffColor = GetQuestDifficultyColor(level);
-					friend.name:SetText(format("%s%s|r", E:RGBToHex(color.r*0.5, color.g*0.5, color.b*0.5), name) .. "," .. format(TOOLTIP_UNIT_LEVEL_CLASS, level, E:RGBToHex(diffColor.r*0.5, diffColor.g*0.5, diffColor.b*0.5)));
+					--diffColor = GetQuestDifficultyColor(level);
+					friend.name:SetText(format("%s%s|r", E:RGBToHex(color.r*0.50, color.g*0.50, color.b*0.50), name) .. ", " .. format(FRIENDS_LEVEL_TEMPLATE, level, class));
 					friend.info:SetText(zone);
 				end
 			end
