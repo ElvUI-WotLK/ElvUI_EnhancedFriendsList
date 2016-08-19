874ded7bd1d219b8519062b1f77844e5efe2f0d1
Bunny67 <zolov67@yandex.ru>
2016-08-19 12:17:42 +0300

    update

--- ElvUI_FriendsListColor/ElvUI_FriendsListColor.lua
+++ ElvUI_FriendsListColor/ElvUI_FriendsListColor.lua
@@ -9,6 +9,7 @@ local unpack, pairs = unpack, pairs;
 local GetFriendInfo = GetFriendInfo;
 local GetNumFriends = GetNumFriends;
 local GetQuestDifficultyColor = GetQuestDifficultyColor;
+local CLASS_ICON_TCOORDS = CLASS_ICON_TCOORDS;
 local CUSTOM_CLASS_COLORS = CUSTOM_CLASS_COLORS;
 local FRIENDS_BUTTON_TYPE_WOW = FRIENDS_BUTTON_TYPE_WOW;
 local LOCALIZED_CLASS_NAMES_FEMALE = LOCALIZED_CLASS_NAMES_FEMALE;
@@ -16,7 +17,10 @@ local LOCALIZED_CLASS_NAMES_MALE = LOCALIZED_CLASS_NAMES_MALE;
 local RAID_CLASS_COLORS = RAID_CLASS_COLORS;
 
 local locale = GetLocale();
-local textFormat = locale == "ruRU" and "%s%s|r, %s%s-го уровня" or locale == "deDE" and "%s%s|r, %sStufe %s" or "%s%s|r, %sLevel %s";
+local textFormat = 
+locale == "ruRU" and "|TInterface\\Glues\\CharacterCreate\\UI-CharacterCreate-Classes:20:20:0:0:64:64:%d:%d:%d:%d|t %s%s|r, %s%d-го уровня" or 
+locale == "deDE" and "|TInterface\\Glues\\CharacterCreate\\UI-CharacterCreate-Classes:20:20:0:0:64:64:%d:%d:%d:%d|t %s%s|r, %sStufe %d" or 
+"|TInterface\\Glues\\CharacterCreate\\UI-CharacterCreate-Classes:20:20:0:0:64:64:%d:%d:%d:%d|t %s%s|r, %sLevel %d";
 
 local locclasses = {};
 for k, v in pairs(LOCALIZED_CLASS_NAMES_MALE) do locclasses[v] = k; end
@@ -25,21 +29,26 @@ for k, v in pairs(LOCALIZED_CLASS_NAMES_FEMALE) do locclasses[v] = k; end
 function mod:FriendsList_Update()
 	if(GetNumFriends() < 1) then return; end
 	local name, level, class, zone, connected, status, note, color, diffColor;
+	local left, right, top, bottom;
 	for i = 1, GetNumFriends() do
 		local friend = _G["FriendsFrameFriendsScrollFrameButton" .. i];
 		if(friend.id and friend.buttonType == FRIENDS_BUTTON_TYPE_WOW) then
 			name, level, class, zone, connected, status, note = GetFriendInfo(friend.id);
-			color = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[locclasses[class]] or RAID_CLASS_COLORS[locclasses[class]];
-			diffColor = GetQuestDifficultyColor(level);
 			if(connected) then
-				friend.name:SetFormattedText(textFormat, E:RGBToHex(color.r, color.g, color.b), name, E:RGBToHex(diffColor.r, diffColor.g, diffColor.b), level);
-				ElvCharacterDB.FriendsListColor[name] = {level, class, zone};
+				if(locclasses[class]) then
+					left, right, top, bottom = unpack(CLASS_ICON_TCOORDS[locclasses[class]]);
+					color = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[locclasses[class]] or RAID_CLASS_COLORS[locclasses[class]];
+					diffColor = GetQuestDifficultyColor(level);
+					friend.name:SetFormattedText(textFormat, (left + 0.015)*64, (right - 0.02)*64, (top + 0.018)*64, (bottom - 0.02)*64, E:RGBToHex(color.r, color.g, color.b), name, E:RGBToHex(diffColor.r, diffColor.g, diffColor.b), level);
+					ElvCharacterDB.FriendsListColor[name] = {level, locclasses[class], zone};
+				end
 			else
 				if(ElvCharacterDB.FriendsListColor[name]) then
 					level, class, zone = unpack(ElvCharacterDB.FriendsListColor[name]);
-					color = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[locclasses[class]] or RAID_CLASS_COLORS[locclasses[class]];
+					left, right, top, bottom = unpack(CLASS_ICON_TCOORDS[class]);
+					color = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[class] or RAID_CLASS_COLORS[class];
 					diffColor = GetQuestDifficultyColor(level);
-					friend.name:SetFormattedText(textFormat, E:RGBToHex(color.r*0.5, color.g*0.5, color.b*0.5), name, E:RGBToHex(diffColor.r*0.5, diffColor.g*0.5, diffColor.b*0.5), level);
+					friend.name:SetFormattedText(textFormat, (left + 0.015)*64, (right - 0.02)*64, (top + 0.018)*64, (bottom - 0.02)*64, E:RGBToHex(color.r*0.5, color.g*0.5, color.b*0.5), name, E:RGBToHex(diffColor.r*0.5, diffColor.g*0.5, diffColor.b*0.5), level);
 					friend.info:SetText(zone);
 				end
 			end
@@ -52,6 +61,13 @@ function mod:Initialize()
 
 	if(not ElvCharacterDB.FriendsListColor) then
 		ElvCharacterDB.FriendsListColor = {};
+	else
+		for n, t in pairs(ElvCharacterDB.FriendsListColor) do
+			local level, class, zone = unpack(t);
+			if(locclasses[class]) then
+				t[2] = locclasses[class];
+			end
+		end
 	end
 	if(E.global.friendsListColor) then
 		ElvCharacterDB.FriendsListColor = E.global.friendsListColor;
