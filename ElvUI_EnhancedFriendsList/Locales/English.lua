116fea0e05a9a8ab05ab687a9f00bc3bd40ee739
bunny67 <zolov6767@gmail.com>
2019-08-17 14:06:26 +0300

    update

--- ElvUI_EnhancedFriendsList/Locales/English.lua
+++ ElvUI_EnhancedFriendsList/Locales/English.lua
@@ -1,5 +1,5 @@
 -- English localization file for enUS and enGB.
-local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0-ElvUI");
 local L = AceLocale:NewLocale("ElvUI", "enUS", true, true);
 if not L then return; end
 
